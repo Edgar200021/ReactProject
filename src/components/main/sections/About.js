330eb43402fd48d7df0140f8d86a81fff9ba364e
Edgar Asatryan <edgarartur2000@mail.ru>
2023-02-24 14:55:23 +0300

    'Final'

--- src/components/main/sections/About.js
+++ src/components/main/sections/About.js
@@ -3,22 +3,24 @@ import iphone from '../img/iphone14.jpg'
 
 import text from '../../../data/Text.json'
 
-
-export default function About(){
-	return(
-		<section className='about'>
-		    <div className='container'>
-           <h2 className='second-title'>ЧТО НОВОГО</h2>
-					 <div className='about__inner'>
-					   <div className="about__texts">
-						 		{text.map((text,index) => <p key={index} className='about__text'>{text.text}</p>)}
-						 </div>
-						 <div className="about__img-box">
-						   <img  src={iphone}/>
-						 </div>
-					 </div>
+export default function About() {
+  return (
+    <section className="about">
+      <div className="container">
+        <h2 className="second-title">ЧТО НОВОГО</h2>
+        <div className="about__inner">
+          <div className="about__texts">
+            {text.map((text, index) => (
+              <p key={index} className="about__text">
+                {text.text}
+              </p>
+            ))}
+          </div>
+          <div className="about__img-box">
+            <img alt="Iphone" src={iphone} />
+          </div>
         </div>
-
-		</section>
-	)
+      </div>
+    </section>
+  )
 }
